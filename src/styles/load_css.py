854bec04aad19209e88232c0b4c397217d7c053d
TimiOlasope <16053081@brookes.ac.uk>
2026-03-06 18:55:08 +0000

    testing load css

--- src/styles/load_css.py
+++ src/styles/load_css.py
@@ -1,8 +1,20 @@
-import streamlit as st
+"""import streamlit as st
 from pathlib import Path
 
 def load_css():
     css_path = Path("src/styles/globalstyle.css")
 
     with open(css_path) as f:
-        st.markdown(f"<style>{f.read()}</style", unsafe_allow_html= True)
+        st.markdown(f"<style>{f.read()}</style", unsafe_allow_html= True)"""
+
+import streamlit as st
+from pathlib import Path
+
+def load_css():
+    css_path = Path(__file__).parent / "globalstyle.css"
+
+    if css_path.exists():
+        with open(css_path) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    else:
+        st.error(f"CSS file not found: {css_path}")
