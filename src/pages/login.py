809ef2f831abbe15515c9163ff82233e53ee4430
TimiOlasope <16053081@brookes.ac.uk>
2026-03-07 20:14:48 +0000

    login page restructure

--- src/pages/login.py
+++ src/pages/login.py
@@ -5,8 +5,19 @@ import streamlit as st
 
 def render():
     col1, col2 = st.columns([1,1])
+    
     with col1:
-        st.markdown('<div class="card">', unsafe_allow_html=True)
+        st.markdown('<div class="login-left">', unsafe_allow_html=True)
+        st.image("src/images/placeholder.jpg", use_container_width=True)
+
+        st.markdown(
+            "<h2 class='hero-title'>MoleMonitor: Tracking your moles.</h2>", unsafe_allow_html=True
+        )
+        st.markdown("<p class='hero-text'>Track changes to your skin moles over time.</p>", unsafe_allow_html=True
+                    )
+        st.markdown("</div>", unsafe_allow_html=True)
+        
+        """st.markdown('<div class="card">', unsafe_allow_html=True)
         st.title("Login")
         
         email = st.text_input("Email")
@@ -22,4 +33,4 @@ def render():
         st.markdown("Create an account")
 
         st.markdown('</div>', unsafe_allow_html=True)
-        st.image("src/images/placeholder.jpg", use_container_width=True)
+        st.image("src/images/placeholder.jpg", use_container_width=True)"""
