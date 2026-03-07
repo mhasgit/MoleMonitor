2a474ce313fb5e3b33cb56ef5a6e8c9905c863ab
TimiOlasope <16053081@brookes.ac.uk>
2026-03-07 20:22:16 +0000

    login page restructure

--- src/pages/login.py
+++ src/pages/login.py
@@ -16,6 +16,22 @@ def render():
         st.markdown("<p class='hero-text'>Track changes to your skin moles over time.</p>", unsafe_allow_html=True
                     )
         st.markdown("</div>", unsafe_allow_html=True)
+
+        st.markdown('<div class="login-right">', unsafe_allow_html=True)
+        st.markdown("<h3>Log in</h3>", unsafe_allow_html=True)
+        email = st.text_input("Email Adress", placeholder="user@example.com")
+        password = st.text_input(
+            "Password",
+            type="password",
+            placeholder="********",
+        )
+        if st.button("Log in"):
+            st.success("Login Attempted")
+        st.markdown("p class='forgot'>Forgot Password?</p>", unsafe_allow_html=True)
+        st.markdown("/div", unsafe_allow_html=True)
+        
+        
+        
         
         """st.markdown('<div class="card">', unsafe_allow_html=True)
         st.title("Login")
