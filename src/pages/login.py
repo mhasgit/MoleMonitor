4aae5ffaace9081f4a020b38b16296685597627f
TimiOlasope <16053081@brookes.ac.uk>
2026-03-07 15:55:23 +0000

    login page further styling

--- src/pages/login.py
+++ src/pages/login.py
@@ -6,7 +6,9 @@ import streamlit as st
 def render():
     col1, col2 = st.columns([1,1])
     with col1:
+        st.markdown('<div class="card">', unsafe_allow_html=True)
         st.title("Login")
+        
         email = st.text_input("Email")
         password = st.text_input(
             "Password",
@@ -16,6 +18,7 @@ def render():
         if st.button("Login"):
             st.success("Login Attempted")
 
-        st.markdown("Forgot Password?")
+        st.markdown("[Forgot Password?](#)")
         st.markdown("Create an account")
 
+        st.markdown('</div>', unsafe_allow_html=True)
