b3e5df4eb39be3aa1332ce4793f5d3ea267eff98
TimiOlasope <16053081@brookes.ac.uk>
2026-03-07 19:56:25 +0000

    login page image placeholder

--- src/pages/login.py
+++ src/pages/login.py
@@ -22,4 +22,4 @@ def render():
         st.markdown("Create an account")
 
         st.markdown('</div>', unsafe_allow_html=True)
-        st.image("images/placeholder.jpg", use_container_width=True)
+        st.image("src/images/placeholder.jpg", use_container_width=True)
