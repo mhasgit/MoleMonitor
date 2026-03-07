f85aa687dd7b21d09bbd3cca4e3cb63c6e67179d
TimiOlasope <16053081@brookes.ac.uk>
2026-03-07 15:47:21 +0000

    login page

--- src/pages/login.py
+++ src/pages/login.py
@@ -3,7 +3,19 @@
 import streamlit as st
 
 
-def render() -> None:
-    st.title("Login")
-    st.info("To be implemented by imogen")
+def render():
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.title("Login")
+        email = st.text_input("Email")
+        password = st.text_input(
+            "Password",
+            type="password"
+        )
+
+        if st.button("Login"):
+            st.success("Login Attempted")
+
+        st.markdown("Forgot Password?")
+        st.markdown("Create an account")
 
