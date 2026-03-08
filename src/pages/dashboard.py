cfc6bcb4acac14633d62fc29ebe481ce9e39e7a7
TimiOlasope <16053081@brookes.ac.uk>
2026-03-08 19:27:05 +0000

    dashboard page

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -12,9 +12,35 @@ def render() -> None:
     col1, col2, col3 = st.columns(3)
 
     with col1:
+        st.markdown('<div class="card">',unsafe_allow_html=True)
+        st.markdown('p class="card-title">Total Images</p', unsafe_allow_html=True)
+        st.markdown('p class="card-value">24</p>', unsafe_allow_html=True)
+        st.markdown('</div', unsafe_allow_html=True)
+    
+    with col2:
+        st.markdown('<div class="card">', unsafe_allow_html=True)
+        st.markdown('p class="card-title">Moles Tracked</p', unsafe_allow_html=True)
+        st.markdown('p class="card-value">12</p>', unsafe_allow_html=True)
+        st.markdown('</div', unsafe_allow_html=True)
+
+    with col3:
+        st.markdown('<div class="card">', unsafe_allow_html=True)
+        st.markdown('p class="card-title">Mole Changes</p', unsafe_allow_html=True)
+        st.markdown('p class="card-value">1</p>', unsafe_allow_html=True)
+        st.markdown('</div', unsafe_allow_html=True)
+
+    st.markdown('<div class="card">', unsafe_allow_html=True)
+    st.subheader("Upload New Image")
+    uploaded_img = st.file_uploader("Upload a skin image")
+    if uploaded_img:
+        st.success("Image successfuly uploaded")
+    
+    st.markdown('</div>', unsafe_allow_html=True)
+
+    """with col1:
         st.metric("Mole Checks: ", "")
     with col2:
         st.metric("Changes Detected: ", "")
     with col3:
-        st.metric("Latest Upload: ", "")
+        st.metric("Latest Upload: ", "")"""
 
