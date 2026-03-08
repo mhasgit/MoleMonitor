9280df7cbadd5ae6ac526aee1e9ff33c588223ba
TimiOlasope <16053081@brookes.ac.uk>
2026-03-08 19:29:15 +0000

    dashboard

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -13,20 +13,20 @@ def render() -> None:
 
     with col1:
         st.markdown('<div class="card">',unsafe_allow_html=True)
-        st.markdown('p class="card-title">Total Images</p', unsafe_allow_html=True)
-        st.markdown('p class="card-value">24</p>', unsafe_allow_html=True)
+        st.markdown('<p class="card-title">Total Images</p', unsafe_allow_html=True)
+        st.markdown('<p class="card-value">24</p>', unsafe_allow_html=True)
         st.markdown('</div', unsafe_allow_html=True)
     
     with col2:
         st.markdown('<div class="card">', unsafe_allow_html=True)
-        st.markdown('p class="card-title">Moles Tracked</p', unsafe_allow_html=True)
-        st.markdown('p class="card-value">12</p>', unsafe_allow_html=True)
+        st.markdown('<p class="card-title">Moles Tracked</p', unsafe_allow_html=True)
+        st.markdown('<p class="card-value">12</p>', unsafe_allow_html=True)
         st.markdown('</div', unsafe_allow_html=True)
 
     with col3:
         st.markdown('<div class="card">', unsafe_allow_html=True)
-        st.markdown('p class="card-title">Mole Changes</p', unsafe_allow_html=True)
-        st.markdown('p class="card-value">1</p>', unsafe_allow_html=True)
+        st.markdown('<p class="card-title">Mole Changes</p', unsafe_allow_html=True)
+        st.markdown('<p class="card-value">1</p>', unsafe_allow_html=True)
         st.markdown('</div', unsafe_allow_html=True)
 
     st.markdown('<div class="card">', unsafe_allow_html=True)
